dc0d0f64d8b22f7a21417ba9f29efc6f188625e6
meghasyam1997 <meghasyam01@gmail.com>
2023-09-12 12:00:33 +0530

    making the variables dynamic

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_ssm_parameter" "parameters" {
   count = length(var.parameters)
   name  = var.parameters[count.index].name
   type  = "String"
-  value = var.parameters[count.index].name
+  value = var.parameters[count.index].value
   key_id = "f2e4231a-4405-4fcc-9d18-bb7c5e81348f"
 }
